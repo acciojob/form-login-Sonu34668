a03bb0f5e1ce021562fbe18eea9627234d022e98
Sonu34668 <130197661+Sonu34668@users.noreply.github.com>
2023-06-28 21:12:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 // const form1 = document.getElementById("form1");
-function getFormvalue() {
+// function getFormvalue() {
     //Write your code here
 	
-	let firstName = getElementById("fname").value;
-	let lastName = getElementById("lname").value;
+	// let firstName = getElementById("fname").value;
+	// let lastName = getElementById("lname").value;
 	alert("First Name: "+ fname + "\nLast Name: " + lname);
-}
+// }
 // form1.addEventListener("submit", getFormvalue);
