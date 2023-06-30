279e17514f0239c68d8ff7fbd5a28dfaba9bfc3f
Sonu34668 <130197661+Sonu34668@users.noreply.github.com>
2023-06-30 22:49:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
-// Get the form element
-const myForm = document.getElementById("myForm");
+function getFormvalue() {
+    function getFormvalue() {
+    //Write your code here
+	
+		this.alert("hello");
+	const form=document.getElementById("form1");
 
-// Define the getFormValue function
-function getFormValue(event) {
-  event.preventDefault(); // Prevent form submission
-  let firstName = document.getElementById("firstName").value;
-  let lastName = document.getElementById("lastName").value;
-  alert("First Name: " + firstName + "\nLast Name: " + lastName);
+	form.addEventListener("submit",(event) => {
+		
+		const formElement = event.target;
+		let firstName=formElement["fname"].value.trim(),
+		let lastName=formElement["lname"].value.trim(),
+			
+	})
 }
 
-// Add event listener to the form
-myForm.addEventListener("submit", getFormValue);
+}
